845a71b16cc55ab3575024fd140528494cd4a5bf
Brandon Wilhite <brandonjwilhite@gmail.com>
2014-09-28 07:32:29 -0400

    very create and update wired

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var uuid = require('node-uuid');
+
 
 var app = express();
 
@@ -29,9 +30,108 @@ app.get('/', function(req, res) {
     res.render('index.html');
   });
 
-app.post('/Entity1', function(request, response){
-  console.log(request.body);      // your JSON
-  response.send(request.body);    // echo the result back
+
+
+
+function createEntity(_entity){
+    var __id = uuid.v4();
+
+    _entity.Id = __id;
+
+    return {
+        eventId: __id,
+        eventType: 'ES.EntityCollection.EntityCreatedEvent<Entity1>',
+        data: {
+            orgName: "Org1",
+            entityName: "Entity1",
+            entity: _entity,
+            metadata: 'eventstore metadata here'
+        }
+    };
+}
+
+function updateEntity(_entity){
+    var __id = uuid.v4();
+
+    return {
+        eventId: __id,
+        eventType: 'ES.EntityCollection.EntityUpdatedEvent<Entity1>',
+
+        data: {
+            orgName: "Org1",
+            entityName: "Entity1",
+            entity: _entity,
+            metadata: 'eventstore metadata here'
+        }
+    };
+}
+
+var pushToStream = function(msg){
+
+    var serializedMsg = JSON.stringify(msg);
+    var headers = {
+        'Content-Type': 'application/vnd.eventstore.events+json',
+        'Content-Length': serializedMsg.length
+    };
+
+    var options = {
+      host: '192.168.164.142',
+      port: 2113,
+      path: '/streams/Sandbox-Entity1-f',
+      method: 'POST',
+      headers: headers,
+      auth: 'admin:changeit'
+    };
+
+    // Setup the request.  The options parameter is
+    // the object we defined above.
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf-8');
+
+      var responseString = '';
+
+      res.on('data',function(){
+          console.log('data');
+      });
+
+      res.on('end', function(data) {
+          console.log('entity create pushed onto stream');
+      });
+    });
+
+    req.on('error', function(e) {
+      // TODO: handle error.
+      console.log('error');
+    });
+
+    req.write(serializedMsg);
+    req.end();
+
+};
+
+
+app.post('/Entity1', function(req, res){
+  console.log('create: ' +  req.body);
+
+  var _entity = createEntity(req.body);
+
+  var _msg = [_entity];
+
+  pushToStream(_msg);
+
+  res.send(_entity.id);
+});
+
+app.put('/Entity1', function(req, res){
+  console.log('update: ' + req.body);
+
+  var _entity = updateEntity(req.body);
+
+  var _msg = [_entity];
+
+  pushToStream(_msg);
+
+  res.send(_entity.id);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
--- event.js
+++ event.js
@@ -1,8 +1,6 @@
 var uuid = require('node-uuid');
 var http = require('http');
 
-var _id = uuid.v4();
-
 function createEntity(_entity){
     var __id = uuid.v4();
 
@@ -36,56 +34,62 @@ function updateEntity(_entity){
     };
 }
 
-/*
-var msg = [createEntity({
+var pushToStream = function(msg){
+
+    var serializedMsg = JSON.stringify(msg);
+    var headers = {
+        'Content-Type': 'application/vnd.eventstore.events+json',
+        'Content-Length': serializedMsg.length
+    };
+
+    var options = {
+      host: '192.168.164.141',
+      port: 2113,
+      path: '/streams/Sandbox-Entity1-f',
+      method: 'POST',
+      headers: headers,
+      auth: 'admin:changeit'
+    };
+
+    // Setup the request.  The options parameter is
+    // the object we defined above.
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf-8');
+
+      var responseString = '';
+
+      res.on('data',function(){
+          console.log('data');
+      });
+
+      res.on('end', function(data) {
+          console.log('entity create pushed onto stream');
+      });
+    });
+
+    req.on('error', function(e) {
+      // TODO: handle error.
+      console.log('error');
+    });
+
+    req.write(serializedMsg);
+    req.end();
+
+};
+
+var _msg = [createEntity({
     WhichEntity: 5,
     Name: "Fenton",
     _metadata: { }
 })];
-*/
 
+pushToStream(_msg);
+
+/*
 var msg = [updateEntity({
     Id: 'a3a88bf3-9a76-42e0-afff-bccc8b39bf9f',
     WhichEntity: 4,
     Name: "Sandy",
     _metadata: { }
 })];
-
-var serializedMsg = JSON.stringify(msg);
-var headers = {
-    'Content-Type': 'application/vnd.eventstore.events+json',
-    'Content-Length': serializedMsg.length
-};
-
-var options = {
-  host: '192.168.164.140',
-  port: 2113,
-  path: '/streams/Sandbox-Entity1-f',
-  method: 'POST',
-  headers: headers,
-  auth: 'admin:changeit'
-};
-
-// Setup the request.  The options parameter is
-// the object we defined above.
-var req = http.request(options, function(res) {
-  res.setEncoding('utf-8');
-
-  var responseString = '';
-
-  res.on('data',function(){
-      console.log('data');
-  });
-
-  res.on('end', function(data) {
-      console.log('end');
-  });
-});
-
-req.on('error', function(e) {
-  // TODO: handle error.
-  console.log('error');
-});
-
-req.write(serializedMsg);
-req.end();
+*/
