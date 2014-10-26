1b7294d366961822cc692f0e614fcff656a07fda
JediMindtrick <brandonjwilhite@gmail.com>
2014-10-26 06:24:30 -0400

    config determined by environment variable

--- config.js
+++ config.js
@@ -1,42 +1,27 @@
-//ProcessControl gets ports 2000
-//ProcessControl is all http
-//ProcessControl is just an idea at the moment....
+var aws = require('./awsconfig'),
+local = require('./localconfig');
 
+var _mapConfig = function(cfg){
+	for (var prop in cfg){
+		if(cfg.hasOwnProperty(prop)){
+			exports[prop] = cfg[prop];			
+		}
+	}
+};
 
-//Http host+port are for both http and websocket (where appropriate)
-
-//input layer
-//this serves up our html, but events then go directly to the stream via websockets
-exports.webServerHttpHost = 'localhost';
-exports.webServerHttpPort = 3000;
-
-//stream
-//this is where new events come in via websocket, they go out to the blp via zmq
-exports.eventServerHttpHost = 'localhost';
-exports.eventServerHttpPort = 4000;
-exports.eventServerStoreContainer = './streamStore';
-
-//blp, ports 5000
-//this is where new events come in via zmq, they go out to the real time store via zmq
-exports.blpServerZmqHost = '127.0.0.1';
-exports.blpServerZmqPort = 5000;
-
-//store
-//this is where model updates come in via zmq, they go out to all subscribers via websockets
-exports.realTimeStoreHttpHost = 'localhost';
-exports.realTimeStoreHttpPort = 6001;
-exports.realTimeStoreZmqHost = '127.0.0.1';
-exports.realTimeStoreZmqPort = 6500;
-exports.realTimeStoreContainer = './modelStore';
-
-//TESTING CONFIGS
-exports.testStreamName = 'AppTestStream';
-
-//PERF TESTING CONFIGS
-//client - general config
-exports.perfRoundtrip = true;
-exports.perfSendOnly = false;
-
-//server - general config
-exports.perfServerSocketsOnly = false;
-exports.connectToStoreViaSockets = false;
+if(!process.env.HYPERLOOP_ENV){
+	_mapConfig(local);
+}else{
+	var _env = process.env.HYPERLOOP_ENV.toLowerCase();
+	switch(_env){
+		case 'aws':
+			_mapConfig(aws);
+			break;
+		case 'local':
+			_mapConfig(local);
+			break;
+		default:
+			_mapConfig(local);
+			break;
+	}
+}
---
+++ localconfig.js
@@ -0,0 +1,42 @@
+//ProcessControl gets ports 2000
+//ProcessControl is all http
+//ProcessControl is just an idea at the moment....
+
+
+//Http host+port are for both http and websocket (where appropriate)
+
+//input layer
+//this serves up our html, but events then go directly to the stream via websockets
+exports.webServerHttpHost = 'localhost';
+exports.webServerHttpPort = 3000;
+
+//stream
+//this is where new events come in via websocket, they go out to the blp via zmq
+exports.eventServerHttpHost = 'localhost';
+exports.eventServerHttpPort = 4000;
+exports.eventServerStoreContainer = './streamStore';
+
+//blp, ports 5000
+//this is where new events come in via zmq, they go out to the real time store via zmq
+exports.blpServerZmqHost = '127.0.0.1';
+exports.blpServerZmqPort = 5000;
+
+//store
+//this is where model updates come in via zmq, they go out to all subscribers via websockets
+exports.realTimeStoreHttpHost = 'localhost';
+exports.realTimeStoreHttpPort = 6001;
+exports.realTimeStoreZmqHost = '127.0.0.1';
+exports.realTimeStoreZmqPort = 6500;
+exports.realTimeStoreContainer = './modelStore';
+
+//TESTING CONFIGS
+exports.testStreamName = 'AppTestStream';
+
+//PERF TESTING CONFIGS
+//client - general config
+exports.perfRoundtrip = true;
+exports.perfSendOnly = false;
+
+//server - general config
+exports.perfServerSocketsOnly = false;
+exports.connectToStoreViaSockets = false;
