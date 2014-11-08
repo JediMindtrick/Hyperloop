6a12f600ea3b86b16544e7591fae2ac8bb7e8959
Brandon Wilhite <brandonjwilhite@gmail.com>
2014-11-07 22:27:53 -0500

    basic perfRun.js in place

--- Perf/perfRun.js
+++ Perf/perfRun.js
@@ -4,68 +4,104 @@ var now = function(){
 	return (new Date()).getTime();
 };
 
+//THESE ALL SEND STATS TO REPORTING STREAM
 //all.report
 var createHeadOfStream = function(report){
-
+	
 };
 
 var reportRunSet = function(set){
-	
+
 };
 
 var reportPerfRunResults = function(all){
 
 };
 
+//GATHER STATISTICS AND ANALYSIS
+var generateReport = function(report,dataset){
+	var toReturn = report;
+
+	//gather some stats over dataset
+
+	return toReturn;
+};
+
+var statData = function(data){
+	var toReturn = data;
+
+	return data;
+};
+
+/*
+To make reusable we also need
+where to save
+statData function
+generateReport function
+*/
 exports.create = function(data){
 
+	var obj = {
+		finished: false
+	};
+
 	var currentSet = {
-		report: {}
+		report: {},
 		data: []
 	};
 
 	var all = {
-		report: { 
+		report: {
 			startTime: now(),
-			endTime: null
+			endTime: null,
 			received: 1,
 			runId: uuid.v4(),
 			max: data._metadata.runSize
 		},
-		dataPoints: []
+		sets: [],
+		data: []
 	};
 
-	//create head of stream
-
-	var generateReport = function(report,dataset){
-		var toReturn = report;
-
-		//gather some stats over dataset
-
-		return toReturn;
-	};
+	createHeadOfStream(all.report);
 
     var checkEnd = function(data){
     	//this is the end
     	if((report.max && received >= report.max)
     		|| data._metadata.endPerfRun){
-    		report.endTime = now();
+    		all.report.endTime = now();
+			obj.finished = true;
     		closeCurrentSet();
-    		//getAllStats();
-    		//saveAllStats();
-    		//saveReport();
+			all.report = generateReport(all.report,all.data);
+			reportPerfRunResults(all);
     	}
-
     };
 
     var closeCurrentSet = function(){
-    	dataPoints.concat(currentSet.data);	
-    	generateReport
+		currentSet.report.endTime = now();
+    	all.data.concat(currentSet.data);
+    	currentSet.report = generateReport(currentSet.report,currentSet.data);
+		reportRunSet(currentSet);
     };
+
+	var openCurrentSet = function(){
+
+		currentSet = {
+			report: {
+				startTime: now(),
+				endTime: null,
+				setId: uuid.v4()
+			},
+			data: []
+		};
+
+		all.sets.push(currentSet.report.setId);
+	};
 
     var _perf = function(data){
 
-    	currentSet.push(data);
+		var toAdd = statData(data);
+
+    	currentSet.push(toAdd);
 	    received++;
 
 	    if(received === 1){
@@ -81,8 +117,5 @@ exports.create = function(data){
 	    checkEnd(data);
     };
 
-
-	return {
-		finished: false		
-	};	
+	return obj;
 };
